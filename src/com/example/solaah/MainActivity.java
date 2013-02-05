574b844e2cd9b8050fcac2181430a4efead64137
Abdulfatai Popoola <abdulapopoola@yahoo.com>
2013-02-05 19:58:56 +0400

    Db moved into app

--- src/com/example/solaah/MainActivity.java
+++ src/com/example/solaah/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.solaah;
 
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.database.SQLException;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +13,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		DataBaseHelper myDbHelper = new DataBaseHelper(this);
+		myDbHelper = new DataBaseHelper(this);
+
+		try {
+			myDbHelper.createDataBase();
+		} catch (IOException ioe) {
+			throw new Error("Unable to create database");
+		}
+
+		try {
+			myDbHelper.openDataBase();
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
 	}
 
 	@Override
@@ -18,5 +36,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
 }
