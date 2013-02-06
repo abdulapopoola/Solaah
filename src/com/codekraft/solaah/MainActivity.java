25e66f5843a2e80c06b5ccb4d5a8355dd571812b
Abdulfatai Popoola <abdulapopoola@yahoo.com>
2013-02-06 19:51:03 +0400

    Fixed db issues and refactored structure

---
+++ src/com/codekraft/solaah/MainActivity.java
@@ -0,0 +1,41 @@
+package com.codekraft.solaah;
+
+import java.io.IOException;
+
+import com.codekraft.data.DataBaseHelper;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.database.SQLException;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		DataBaseHelper myDbHelper = new DataBaseHelper(this);
+		myDbHelper = new DataBaseHelper(this);
+
+		try {
+			myDbHelper.createDataBase();
+		} catch (IOException ioe) {
+			throw new Error("Unable to create database");
+		}
+
+		try {
+			myDbHelper.openDataBase();
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+}
