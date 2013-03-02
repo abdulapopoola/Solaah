8ddc1b0554558971a6837383b1f74dbf2dd9ae7a
Abdulfatai Popoola <abdulapopoola@yahoo.com>
2013-03-02 20:10:36 +0400

    List Adapter buggy

--- src/com/codekraft/solaah/MainActivity.java
+++ src/com/codekraft/solaah/MainActivity.java
@@ -5,16 +5,25 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import android.app.Activity;
+import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.codekraft.data.SQLDbAdapter;
 
 public class MainActivity extends Activity {
 	private static final String TAG = "salaahApp";
 	private SQLDbAdapter myDbHelper;
+	protected String[] from;
+	protected int[] to;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +36,19 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			Log.e("DB FAILED", TAG);
 		}
-		//myDbHelper.getReadableDatabase();
 
 		String today = getTodayDate();
 		getPrayerTimesForDate(today);
+		
+		//From is the column name in your cursor where you're getting the data
+		//to is the id of the view it will map to
+		from = new String[]{"Month"};
+		to = new int[]{R.id.text1};
+		
+		Cursor cursor = myDbHelper.getTimingsForDate(today);
+		CustomCursorAdapter adapter = new CustomCursorAdapter(this, R.layout.solaah_row, cursor, from, to);
+	    ListView listView = (ListView) findViewById(R.id.listView1);
+		listView.setAdapter(adapter);
 	}
 
 	@Override
@@ -55,17 +73,64 @@ public class MainActivity extends Activity {
 	private String getPrayerTimesForDate(String date)
 	{
 		Cursor cursor = myDbHelper.getTimingsForDate(date);
-		int colCount = cursor.getColumnCount();
-		Log.e(""+colCount, TAG);
 		cursor.moveToFirst();
+		//int colCount = cursor.getColumnCount();		
 		String var1 = cursor.getString(3);
-		Log.e(var1, TAG);
-//		for (int i=0; i < colCount; i++) {
-//			  String var1 = cursor.getString(i);
-//			  Log.i(var1, "Hello");
-//		}
+		
+		
 		
 		return "";
 	}
+	
+	protected class CustomCursorAdapter extends SimpleCursorAdapter  {
+        private int layout; 
+        private LayoutInflater inflater;
+        private Context context;
 
+        public CustomCursorAdapter (Context context, int layout, Cursor c, String[] from, int[] to) {
+            super(context, layout, c, from, to);
+            this.layout = layout;
+            this.context = context;
+            inflater = LayoutInflater.from(context);
+
+        }
+
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            Log.i("NewView", "TUNTUTN");
+
+            View v = inflater.inflate(R.layout.solaah_row, parent, false);
+
+            return v;
+        }
+
+        @Override
+        public void bindView(View v, Context context, Cursor c) {
+                    //1 is the column where you're getting your data from
+            String name = c.getString(1);
+            /**
+             * Next set the name of the entry.
+             */
+            TextView name_text = (TextView) v.findViewById(R.id.text1);
+            if (name_text != null) {
+                name_text.setText(name);
+            }   
+        }
+	
+//	private void fillData(String date)
+//	{
+//        // Get all of the notes from the database and create the item list
+//        Cursor c = myDbHelper.getTimingsForDate(date);
+//        startManagingCursor(c);
+//
+//        String[] from = new String[] { "Fajr" };
+//        int[] to = new int[] { R.id.text1 };
+//        
+//        // Now create an array adapter and set it to display using our row
+//        SimpleCursorAdapter notes =
+//            new SimpleCursorAdapter(this, R.layout.solaah_row, c, from, to);
+//        setListAdapter(notes);
+//    }
+	}
 }
